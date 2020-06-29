c6d5427ed0b9b6a7e6fa78aaa2683541769643d0
Arthurgallina1 <arthur.gallina@gmail.com>
2020-06-29 16:20:12 -0300

    add gif

--- src/App.js
+++ src/App.js
@@ -3,33 +3,33 @@ import React, { useEffect, useState } from "react";
 import { Cards, Chart, CountryPicker, Table } from "./Components";
 import styles from "./App.module.css";
 import { fetchData } from "./api";
-import CoronaLogo from "./imgs/covid.jpg";
+import CoronaLogo from "./imgs/covid.png";
 
 export default function App() {
-    const [data, setData] = useState([]);
-    const [country, setCountry] = useState("");
+  const [data, setData] = useState([]);
+  const [country, setCountry] = useState("");
 
-    const handleCountryChange = async (country) => {
-        const fetchedData = await fetchData(country);
-        setData(fetchedData);
-        setCountry(country);
-    };
+  const handleCountryChange = async (country) => {
+    const fetchedData = await fetchData(country);
+    setData(fetchedData);
+    setCountry(country);
+  };
 
-    useEffect(() => {
-        async function getInfo() {
-            const data = await fetchData();
-            setData(data);
-        }
-        getInfo();
-    }, []);
+  useEffect(() => {
+    async function getInfo() {
+      const data = await fetchData();
+      setData(data);
+    }
+    getInfo();
+  }, []);
 
-    return (
-        <div className={styles.container}>
-            <img src={CoronaLogo} alt='' height={150} />
-            <Cards data={data} />
-            <CountryPicker handleCountryChange={handleCountryChange} />
-            <Chart country={country} data={data} />
-            <Table country={country} data={data} />
-        </div>
-    );
+  return (
+    <div className={styles.container}>
+      {/* <img src={CoronaLogo} alt="" height={80} /> */}
+      <Cards data={data} />
+      <CountryPicker handleCountryChange={handleCountryChange} />
+      <Chart country={country} data={data} />
+      {/* <Table country={country} data={data} /> */}
+    </div>
+  );
 }
